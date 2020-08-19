669032c8aefd8b16a184005cadd34076abd19e27
Sean Li <li.saifahn@gmail.com>
2020-08-19 12:24:33 +0900

    Add composition API, basic counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="increaseCount">Increase count</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from '@vue/composition-api'
+export default defineComponent ({
+  setup() {
+    const count = ref(0);
+    function increaseCount() {
+      count.value += 1;
+    }
+    return { count, increaseCount }
+  }
+})
+</script>
